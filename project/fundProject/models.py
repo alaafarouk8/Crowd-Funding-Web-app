c2dfe652724d9cab2a48b6bf42bcfece241a1d4c
Ahmed Hassanein <ahassaneina7@gmail.com>
2022-02-13 02:33:01 +0200

    create project model1.1

--- project/fundProject/models.py
+++ project/fundProject/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     details = models.TextField()
-    category = models.CharField(choices=['children', 'Entrepreneurs', 'Medical', 'Technology'])
+    category = models.CharField()
     project_pictures = models.ImageField()
     total_target = models.ImageField()
-    tags = models.(choices=['children', 'Entrepreneurs', 'Medical', 'Technology'])
+    tags = models.CharField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    project_show=models.BooleanField(default=0)
+
+class Images(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to="media/projects/img")
+
