0345bf29dbc4aaae30958ababfba22fd736b13d0
ALaa Farouk <alaafar19@gmail.com>
2022-02-18 19:01:16 +0200

    search function done

--- project/fundproject/views.py
+++ project/fundproject/views.py
@@ -1,8 +1,11 @@
+from csv import QUOTE_NONE, QUOTE_NONNUMERIC
 from multiprocessing import context
+from queue import Empty
 from unicodedata import category
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect, HttpResponse
 from .models import *
+from django.db.models import Q  
 
 #######################################
 # create project
@@ -52,13 +55,11 @@ def list_project(request):
     return render(request, 'project/project_list.html', context)
 
 
-
-# home 
+# --------------------------------- HOME Page -----------------------------------------
 def home(request):
    
     latestProjects = Project.objects.values('project_id').order_by('start_date')[0:5]
     FeaturedProjects = Project.objects.values('project_id')[0:5]
-
     latestProjectsList = []
     for project in latestProjects:
         latestProjectsList.append(Images.objects.filter(project_id=project['project_id']))
@@ -75,6 +76,8 @@ def home(request):
     }
     return render(request, "home.html", context)
 
+# --------------------------------- List of Project in Category Page -----------------------------------------
+
 def project_list(request, id):
     project_list = []
     category = Categories.objects.get(category_id=id)
@@ -88,3 +91,26 @@ def project_list(request, id):
     }
     print(project_list)
     return render(request, 'list_projects.html',context )
+
+# --------------------------------- Search Function-----------------------------------------
+# we use Q objects to make more complex queries following
+# Use | (OR) operator to search for only one field. For example, when searching title | content, both don’t have to be true, only one is okay, when searching for a word such as “python”, so it(python) doesn’t have to be contained in both title and content fields
+#https://stackpython.medium.com/django-search-with-q-objects-tutorial-9c701db74e0e
+def search(request):
+    project_list = []
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        if query is not None:
+            search = Q(title__icontains=query) | Q(tags__tag_name__icontains=query)
+            results = Project.objects.filter(search).values('project_id')
+            for project in results:
+                project_list.append(Images.objects.filter(project_id=project['project_id']))
+            
+            return render(request, 'search_project.html', {'project_list': project_list})
+
+        else:
+            return render(request, 'search_project.html')
+    else:
+        return render(request, 'home.html')
+
+
